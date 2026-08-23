595eda46e2074bec544c8f832d84c8276ccc6486
ganesh ghodke <ganeshghodke542@gmail.com>
2026-08-23 11:43:23 +0530

    Insertion sorting

---
+++ src/Sorting/insertionSorting.java
@@ -0,0 +1,26 @@
+package Sorting;
+
+public class insertionSorting {
+    public static void main(String[] args) {
+
+        int arr[] = {4, 3, 5, 1, 2};
+
+        for (int i = 1; i < arr.length; i++) {
+
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(i + " = " + arr[i]);
+        }
+    }
+}
