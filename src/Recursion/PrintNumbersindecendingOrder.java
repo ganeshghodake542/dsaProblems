e48bb3e8c0ab7f93a1d8ca69a6dd1af38fbd60a0
ganesh ghodke <ganeshghodke542@gmail.com>
2026-08-30 16:04:20 +0530

    printOrder in Decending prder

---
+++ src/Recursion/PrintNumbersindecendingOrder.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class PrintNumbersindecendingOrder {
+
+    public static void printNum(int n){
+        if (n== 5) {
+            return;
+        }
+        System.out.println(n);
+        printNum(n+1);
+    }
+
+    public static void main(String[] args) {
+        int n = 0;
+        printNum(n);
+        
+    }
+}
