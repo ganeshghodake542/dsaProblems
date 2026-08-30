df60f412e5157902db1087833b4fa65bbb6d7f8d
ganesh ghodke <ganeshghodke542@gmail.com>
2026-08-30 16:11:05 +0530

    Sum of n numbers

---
+++ src/Recursion/addtionOfNum.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class addtionOfNum {
+
+    public static void printSum(int i , int n , int sum){
+
+        if(i == n){
+            sum += i;
+            System.out.println(sum);
+            return;
+        }
+
+        sum +=i;
+        printSum(i+1, n, sum);
+
+    }
+
+    public static void main(String[] args) {
+        printSum(0, 05, 0);
+        
+    }
+}
