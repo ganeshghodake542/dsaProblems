7b501746e402b0c6ff3be2e11e90a698150a32a0
ganesh ghodke <ganeshghodke542@gmail.com>
2026-09-03 21:09:32 +0530

    FindSubset

---
+++ src/Backtracking/findSubset.java
@@ -0,0 +1,27 @@
+package Backtracking;
+
+
+public class findSubset {
+
+    public static void findSS(String str , int i, String ans){
+        //base condition
+        if(i==str.length()){
+            System.out.println(ans);
+            return;
+        }
+
+
+        //yess
+        findSS(str, i+1, ans+str.charAt(i));
+        //No
+        findSS(str, i+1, ans);
+    }
+
+    public static void main(String[] args) {
+
+        String str = "abc";
+        findSS(str, 0, "");
+        
+    }
+    
+}
