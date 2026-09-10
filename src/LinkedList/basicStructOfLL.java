3d55cdf10024403ee3e5b84b445ae3ae9796f491
ganesh ghodke <ganeshghodke542@gmail.com>
2026-09-10 14:18:20 +0530

    reverse ll

---
+++ src/LinkedList/basicStructOfLL.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+public class basicStructOfLL {
+
+    public static class Node{
+        int data ;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+           basicStructOfLL ll = new basicStructOfLL();
+
+           
+           
+    }
+}
