936d847b670c26529ae9048756a33e9f1bf55793
ganesh ghodke <ganeshghodke542@gmail.com>
2026-09-11 11:28:32 +0530

    Updated Is cycle code

--- src/LinkedList/isCycle.java
+++ src/LinkedList/isCycle.java
@@ -1,15 +1,50 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
 public class isCycle {
+
     public boolean hasCycle(ListNode head) {
+
         ListNode slow = head;
         ListNode fast = head;
 
-        while(fast != null && fast.next != null){
+        while (fast != null && fast.next != null) {
+
             slow = slow.next;
             fast = fast.next.next;
-            if(slow == fast){
+
+            if (slow == fast) {
                 return true;
             }
         }
+
         return false;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+
+        fourth.next = second;
+
+        isCycle obj = new isCycle();
+
+        boolean result = obj.hasCycle(head);
+
+        System.out.println(result);
     }
 }
