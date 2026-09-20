ff402cd9d7d890d742e548c62351ee1b6b894d10
ganesh ghodke <ganeshghodke542@gmail.com>
2026-09-20 14:07:24 +0530

    count distinct elements

---
+++ src/HashSet/CountDistnElement.java
@@ -0,0 +1,20 @@
+package HashSet;
+import java.util.*;
+
+public class CountDistnElement {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,1,3,4,2,6}; // size = 5
+        
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0 ; i< arr.length ; i++){
+            set.add((arr[i]));
+        }
+
+        System.out.println(set.size());
+
+    }
+    
+}
