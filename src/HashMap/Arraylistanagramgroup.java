4f5ee9c7c47a8d0567fcb276496237a8e25b1e7a
ganesh ghodke <ganeshghodke542@gmail.com>
2026-09-22 15:02:28 +0530

    anagram group

---
+++ src/HashMap/Arraylistanagramgroup.java
@@ -0,0 +1,31 @@
+package HashMap;
+    import java.util.*;
+/**
+ * Arraylistanagramgroup
+ */
+public class Arraylistanagramgroup {
+    public static void main(String[] args) {
+
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+
+            int[] freq = new int[26];
+
+            for (int i = 0; i < str.length(); i++) {
+                freq[str.charAt(i) - 'a']++;
+            }
+
+            String key = Arrays.toString(freq);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(str);
+        }
+    }
+
+}
